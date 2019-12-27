2864fe7db9692591045345b60ebb5b511c01798f
pratikbaid3 <pratikbaid3@gmail.com>
2019-12-27 20:41:23 +0530

    Added firebase realtime database auth function

--- app/src/main/java/com/example/chat_app_template/MainActivity.java
+++ app/src/main/java/com/example/chat_app_template/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.chat_app_template;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +24,26 @@ public class MainActivity extends AppCompatActivity {
     Button Signup;
     Button Fb;
     Button Google;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        FirebaseUser currentUser=mAuth.getCurrentUser();
+        if(currentUser!=null)
+        {
+            //Go to chat home page
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth=FirebaseAuth.getInstance();
 
         emailId=findViewById(R.id.edtEmailIdLogin);
         password=findViewById(R.id.edtPasswordLogin);
@@ -47,6 +68,23 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v)
             {
                 //Login button clicked on the login page
+                mAuth.signInWithEmailAndPassword(emailId.getEditText().getText().toString(), password.getEditText().getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful())
+                                {
+                                    //Signin successful
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } else {
+                                    //Signin failed
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+
+                                // ...
+                            }
+                        });
             }
         });
 
--- app/src/main/java/com/example/chat_app_template/Signup_Page.java
+++ app/src/main/java/com/example/chat_app_template/Signup_Page.java
@@ -1,12 +1,19 @@
 package com.example.chat_app_template;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Signup_Page extends AppCompatActivity {
 
@@ -17,11 +24,15 @@ public class Signup_Page extends AppCompatActivity {
     Button Fb;
     Button Google;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup__page);
+
+        mAuth=FirebaseAuth.getInstance();
 
         emailId=findViewById(R.id.edtEmailSignup);
         password=findViewById(R.id.edtPasswordSignup);
@@ -37,6 +48,23 @@ public class Signup_Page extends AppCompatActivity {
             public void onClick(View v)
             {
                 //Perform signup activity
+                mAuth.createUserWithEmailAndPassword(emailId.getEditText().getText().toString(), password.getEditText().getText().toString())
+                        .addOnCompleteListener(Signup_Page.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful())
+                                {
+                                    //Sign up successful
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } 
+                                else {
+                                    //Signup error
+                                    Toast.makeText(Signup_Page.this, "", Toast.LENGTH_SHORT).show();
+                                }
+
+                                // ...
+                            }
+                        });
             }
         });
     }
