514c9580276a296e95999380f1fb19e297a007ea
pratikbaid3 <pratikbaid3@gmail.com>
2019-12-29 01:19:03 +0530

    Implemented auto login on detecting existing user

--- app/src/main/java/com/example/chat_app_template/MainActivity.java
+++ app/src/main/java/com/example/chat_app_template/MainActivity.java
@@ -33,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser=mAuth.getCurrentUser();
         if(currentUser!=null)
         {
-            /**Intent intent=new Intent(MainActivity.this,Users_List_Chat.class);
+            Intent intent=new Intent(MainActivity.this,Users_List_Chat.class);
             startActivity(intent);
-            finish();**/
+            finish();
         }
     }
 
