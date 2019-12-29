15d121a9d54a7c3beccbe35aaf4574a421f76d41
pratikbaid3 <pratikbaid3@gmail.com>
2019-12-29 22:41:30 +0530

    Generated messages page

---
+++ app/src/main/java/com/example/chat_app_template/Messages_Page.java
@@ -0,0 +1,14 @@
+package com.example.chat_app_template;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Messages_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages__page);
+    }
+}
